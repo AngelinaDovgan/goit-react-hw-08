64c515d605645e6ee534269638ad4ab7585f7506
Angelina <midnightshout03@gmail.com>
2024-05-16 23:01:17 +0300

    7

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,3 +1,37 @@
+import { Field, Formik } from "formik";
+import { useDispatch } from "react-redux";
+
 export default function RegistrationForm() {
-    
+    const dispatch = useDispatch();
+
+    const handleSubmit = (values, actions) => {
+        dispatch(register(values));
+        actions.resetForm();
+    };
+
+    return (
+        <Formik
+            initialValues={{
+                name: "",
+                email: "",
+                password: "",
+            }}
+            onSubmit={handleSubmit}>
+            <Form autoComplete="off">
+                <label>Username
+                    <Field type="text" name="name" />
+                </label>
+
+                <label>
+                    Email
+                    <Field type="email" name="email" />
+                </label>
+                <label>
+                    Password
+                    <Field type="password" name="password" />
+                </label>
+                <button type="submit">Register</button>
+            </Form>
+        </Formik>
+    );
 }
