dc69bafbf5c6de52b4e9528372ca63ffa147944a
Angelina <midnightshout03@gmail.com>
2024-05-16 22:45:25 +0300

    6

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "../../redux/auth/selectors";
+import { logout } from "../../redux/auth/operations";
+
+export default function UserMenu() {
+    const dispatch = useDispatch();
+    const user = useSelector(selectUser);
+
+    const handleLogout = () => {
+        dispatch(logout());
+    };
+
+    return (
+        <div>
+            <p>Welcome, {user.name}</p>
+            <button type="button" onClick={handleLogout}>Logout</button>
+        </div>
+    );
+}
