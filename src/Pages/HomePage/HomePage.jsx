b767417e8d5ee4b0f7037df8f05f0b6bea38ca5f
Angelina <midnightshout03@gmail.com>
2024-05-17 13:40:33 +0300

    11

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import PageTitle from '../../components/PageTitle/PageTitle';
+
+export default function HomePage() {
+    return (
+        <div>
+            <PageTitle>
+                Welcome{" "}
+            </PageTitle>
+            <p>
+                Hello!
+            </p>
+        </div>
+    )
+}
