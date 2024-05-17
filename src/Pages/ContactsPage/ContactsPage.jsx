25af4d0ec39db2690acfeb558301e699f976ba56
Angelina <midnightshout03@gmail.com>
2024-05-17 13:48:09 +0300

    12

---
+++ src/Pages/ContactsPage/ContactsPage.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContacts } from "../../redux/contacts/operations";
+import PageTitle from "../../components/PageTitle/PageTitle";
+import ContactForm from "../../components/ContactForm/ContactForm";
+import ContactList from "../../components/ContactList/ContactList";
+
+export default function ContactsPage() {
+    const dispatch = useDispatch();
+    const isLoading = useSelector(selectLoading);
+
+    useEffect(() => {
+        dispatch(fetchContacts());
+    }, [dispatch]);
+
+    return (
+        <>
+            <PageTitle>Your Contacts</PageTitle>
+            <ContactForm />
+            <div>{isLoading && "Request in progress..."}</div>
+            <ContactList />
+        </>
+);
+}
