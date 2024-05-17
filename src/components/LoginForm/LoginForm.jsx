4bf56c44cea4e7d8da88eafed5aee4bc71a1035a
Angelina <midnightshout03@gmail.com>
2024-05-17 10:58:38 +0300

    form

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,43 @@
+import { useDispatch } from "react-redux";
+import { login } from "../../redux/auth/operations";
+import toast from "react-hot-toast";
+import { Field, Formik } from "formik";
+
+
+export default function LoginForm() {
+    const dispatch = useDispatch();
+
+    const handleSubmit = (values, action) => {
+        dispatch(login(values))
+            .unwrap()
+            .then(response => {
+                toast.success("Yohoo!");
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        actions.resetForm();
+    };
+
+    return (
+        <Formik
+            initialValues={{
+                email: "",
+                password: "",
+            }}
+            onSubmit={handleSubmit}>
+            <Form autocomplete="off">
+                <label>
+                    Email
+                    <Field type="email" name="email" />
+                </label>
+                
+                <label>
+                    Password
+                    <Field type="password" name="password" />
+                </label>
+                <button type="submit">Log In</button>
+            </Form>
+        </Formik>
+    );
+}
